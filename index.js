27e0968411a30ae936716811f34536a2ea911efa
erbroome <ellenehall@gmail.com>
2025-07-25 20:04:45 -0500

    this is the test-my-package for my-first-package

---
+++ index.js
@@ -0,0 +1,4 @@
+import { greet } from 'ellene-hello-lib';
+console.log(greet('World'));
+console.log(greet('Ellene'));
+// This code imports the greet function from the 'ellene-hello-lib' package and logs greetings to the console.
